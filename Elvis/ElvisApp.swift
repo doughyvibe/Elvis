5a3fbcca7685fd6a4998b78b6620614879168e10
Doughy <Doughyvibe@gmail.com>
2025-11-07 18:32:41 +0800

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Doughy on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
